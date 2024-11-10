99e6195ed70178cb7ab9b1d4be175daeb7b3e91f
Walkercito <yoanky2005@gmail.com>
2024-11-10 19:29:15 +0000

    feat: responsive

--- todoapp_frontend/views/login.py
+++ todoapp_frontend/views/login.py
@@ -4,73 +4,99 @@ from todoapp_frontend.styles.styles import Color
 from todoapp_frontend.styles.styles import TextColor
 from todoapp_frontend.styles.styles import Size
 
-
 def login_view() -> rx.Component:
-    return rx.center(
-        rx.card(
-            rx.vstack(
-                rx.box(
-                    rx.center(
-                        rx.icon(
-                            tag="circle-check",
-                            size=64,
-                            color=Color.ICON.value
-                        )
-                    )
-                ),
-                rx.box(
-                    rx.center(
-                        rx.vstack(
-                            rx.heading(
-                                "Todo APP",
-                                color=TextColor.PRIMARY.value
-                            ),
-                            rx.text(
-                                "Organiza tus tareas de manera eficiente",
-                                color=TextColor.SECONDARY.value
-                            ),
-                            align="center",
-                            spacing="1"
-                        ),
-                    ),
-                ),
+    return rx.container(
+        rx.center(
+            rx.card(
                 rx.vstack(
-                    rx.input(
-                        rx.input.slot(
+                    rx.box(
+                        rx.center(
                             rx.icon(
-                                tag="mail",
+                                tag="circle-check",
+                                size=48,  # Fixed size instead of breakpoints
                                 color=Color.ICON.value
                             )
+                        )
+                    ),
+                    rx.box(
+                        rx.center(
+                            rx.vstack(
+                                rx.heading(
+                                    "Todo APP",
+                                    color=TextColor.PRIMARY.value,
+                                    font_size=rx.breakpoints(
+                                        initial="xl",
+                                        sm="2xl",
+                                        lg="3xl"
+                                    )
+                                ),
+                                rx.text(
+                                    "Organiza tus tareas de manera eficiente",
+                                    color=TextColor.SECONDARY.value,
+                                    font_size=rx.breakpoints(
+                                        initial="sm",
+                                        sm="md",
+                                        lg="lg"
+                                    )
+                                ),
+                                align="center",
+                                spacing="1"
+                            ),
                         ),
-                        placeholder="Correo electrónico",
-                        width="100%"
                     ),
-                    rx.input(
-                        rx.input.slot(
-                            rx.icon(
-                                tag="lock",
-                                color=Color.ICON.value
-                            )
+                    rx.vstack(
+                        rx.input(
+                            rx.input.slot(
+                                rx.icon(
+                                    tag="mail",
+                                    color=Color.ICON.value
+                                )
+                            ),
+                            placeholder="Correo electrónico",
+                            width="100%"
                         ),
-                        placeholder="Contraseña",
-                        type="password",
-                        width="100%"
+                        rx.input(
+                            rx.input.slot(
+                                rx.icon(
+                                    tag="lock",
+                                    color=Color.ICON.value
+                                )
+                            ),
+                            placeholder="Contraseña",
+                            type="password",
+                            width="100%"
+                        ),
+                        rx.button(
+                            "Iniciar Sesión",
+                            width="100%"
+                        ),
+                        width="100%",
+                        spacing=Size.DEFAULT.value,
+                        padding=rx.breakpoints(
+                            initial="2",
+                            sm="4",
+                            lg="6"
+                        ),
                     ),
-                    rx.button(
-                        "Iniciar Sesión",
-                        width="100%"
-                    ),
-                    width="100%",
-                    spacing=Size.DEFAULT.value,
-                    padding=Size.SMALL.value,
+                    spacing=Size.LARGE.value,
+                    align="center",
+                    width="100%"
                 ),
-                spacing=Size.LARGE.value,
-                align="center",
-                width="100%"
-            ),
-            height=Size.CARD_HEIGHT.value,
-            width=Size.CARD_WIDTH.value,
-            border_radius=Size.MEDIUM.value,
-            padding=Size.EXTRA_LARGE.value,
-        )
+                height="auto",
+                width=rx.breakpoints(
+                    initial="100%",
+                    sm="80%",
+                    lg=Size.CARD_WIDTH.value
+                ),
+                border_radius=Size.MEDIUM.value,
+                padding=rx.breakpoints(
+                    initial="4",
+                    sm="8",
+                    lg=Size.EXTRA_LARGE.value
+                ),
+            )
+   
+     ),
+        center_content=True,
+        size="2"
     )
